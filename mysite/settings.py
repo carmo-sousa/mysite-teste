23808d88ce83f91fd3e02850ea47d726991e660d
Romulo Sousa <carmosousa20@outlook.com>
2022-09-04 15:31:35 -0400

    Update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*3f-4ms69c_@e6d!8^d5by$c_bl1*sh0bjfw(ql_*mc3(hfb=p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
